1ce61dd8ec3a206bc8d5adebd83aa1edc3c7cfb0
Sai Manoj Kumar Yadlapati <saiyad@microsoft.com>
2019-09-22 19:38:19 +0530

    Remove the option of runtime

--- Template/Deployment/Deployment/Deploy-AzureResourceGroup.ps1
+++ Template/Deployment/Deployment/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'Deployment',
+    [string] $ResourceGroupName = 'AzFnDeployment2',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
